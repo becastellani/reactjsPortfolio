748b69bd8bd6d7e7c9cf75d1caf6aa472a7c4273
becastellani <becastellani10@gmail.com>
2023-04-14 17:17:39 -0300

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+function Inicio (){
+        return (
+            <div id='fundo' class='fundo'>
+                <div id='nomeprincipal' class='nomeprincipal'>
+                    Bernardo Castellani</div>
+            </div>
+        )
+
+}
+export default Inicio

e7ccfa1678bfa7376c0228c60ad54baf4a4e61a2
Bernardo Mantoan Castellani <bernardo.castellani@alunos.bpkedu.com.br>
2023-04-18 20:37:24 -0300

    redes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,8 @@ import styles from './Home.module.css'
 import Logo from './img/instaIcon.png'
 import Logo1 from './img/gitIcon.png'
 import Logo2 from './img/linkedinIcon.png'
-
+import Logo3 from './img/whatss.png'
+import Logo4 from './img/gmaill.png'
 function Inicio (){
         return (
     <div>
@@ -11,7 +12,9 @@ function Inicio (){
         <div id='linha-imagem'>
         <a href="https://www.instagram.com/bernardo_castell/"><img src={Logo}/></a>
         <a href="https://github.com/becastellani"><img src={Logo1}></img></a>
-        <a href="https://www.linkedin.com/in/bernardo-castellani-b515a0203/"><img src={Logo2} ></img></a>
+        <a href=""><img src={Logo2} ></img></a>
+        <a href=""><img src={Logo3} ></img></a>
+        <a href=""><img src={Logo4} ></img></a>
             </div>  
         
     </div>
